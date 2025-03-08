49ff4a6bd180af7cfc5dd8233e235da219c72137
The SunShining <thesunshiningmin@gmail.com>
2025-03-08 12:11:25 -0700

    clean up!?

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,9 +1,9 @@
 import { useContext,useEffect, useState } from 'react';
 
-import NearLogo from '@/assets/near-logo.svg';
+// import NearLogo from '@/assets/near-logo.svg';
 import { NearContext } from '@/wallets/near';
 import { Link } from "react-router";
-import styles from '@/styles/app.module.css';
+// import styles from '@/styles/app.module.css';
 
 export const Navigation = () => {
   const { signedAccountId, wallet } = useContext(NearContext);
@@ -26,7 +26,7 @@ export const Navigation = () => {
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
         <Link to="/">
-          <img src={NearLogo} alt="NEAR" width="30" height="24" className={styles.logo} />
+          {/* <img src={NearLogo} alt="NEAR" width="30" height="24" className={styles.logo} /> */}
         </Link>
         <div className="navbar-nav pt-1">
           <button className="btn btn-secondary" onClick={action}>
