07a9eb162fa4c973fda01535c942f63329529d72
The SunShining <thesunshiningmin@gmail.com>
2025-03-08 07:34:15 -0700

    import { Buffer } from 'buffer';

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { Buffer } from 'buffer';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,9 +15,12 @@ export default defineConfig({
   },
   define: {
     'global': {},
-    'process.env': {}
+    'process.env': {},
+    'Buffer': [Buffer],
+    'global.Buffer': [Buffer]
   },
   optimizeDeps: {
+    include: ['buffer'],
     esbuildOptions: {
       define: {
         global: 'globalThis'
