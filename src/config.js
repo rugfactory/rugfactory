4552af91d4240488866946ce4ce52295cee9c322
The SunShining <thesunshiningmin@gmail.com>
2025-03-10 16:54:28 -0600

    // config

--- src/config.js
+++ src/config.js
@@ -3,6 +3,11 @@ const contractPerNetwork = {
   testnet: 'rug-testing.testnet',
 };
 
+const shitTokenPerNetwork = {
+  mainnet: 'shit-1170.meme-cooking.near',
+  testnet: 'shit-237.factory.v10.meme-cooking.testnet',
+};
+
 export const NetworkId = 'testnet';
 export const RugFactoryContract = contractPerNetwork[NetworkId];
-export const ShitTokenContract = 'shit-237.factory.v10.meme-cooking.testnet';
+export const ShitTokenContract = shitTokenPerNetwork[NetworkId];
