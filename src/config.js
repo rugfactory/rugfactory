43a779484ea25087ad449d45cc7dc4070a1b8066
The SunShining <thesunshiningmin@gmail.com>
2025-03-10 20:38:20 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -8,6 +8,12 @@ const shitTokenPerNetwork = {
   testnet: 'shit-237.factory.v10.meme-cooking.testnet',
 };
 
+const refPoolPerNetwork = {
+  mainnet: 'https://dex.rhea.finance/pool/',
+  testnet: 'https://testnet.ref.finance/pool/',
+};
+
 export const NetworkId = 'testnet';
 export const RugFactoryContract = contractPerNetwork[NetworkId];
 export const ShitTokenContract = shitTokenPerNetwork[NetworkId];
+export const RefPoolUrl = refPoolPerNetwork[NetworkId];
